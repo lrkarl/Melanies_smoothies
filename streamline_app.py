1efa3359513a8206c0bdaace3c79a3695b4374b4
lrkarl <lkarl@revescoproperties.com>
2025-09-11 15:07:15 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -13,6 +13,7 @@ st.write(
 
 name_on_order= st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
@@ -22,7 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -34,14 +35,14 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
+        ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        t.json(smoothiefroot_response.json())
 
     st.write(ingredients_string)
 
@@ -49,12 +50,8 @@ if ingredients_list:
         values ('""" + ingredients_string.strip() + """', '"""+name_on_order+"""')"""
 
     st.write(my_insert_stmt)
-
-
-    #st.write(my_insert_stmt)
-    time_to_insert = st.button ('Submit Order')
-
+    time_to_insert = st.button('Submit Order')
+    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-
-        st.success('Your Smoothie is ordered!',icon="✅" )  
+        st.success('Your Smoothie is ordered!', icon="✅")
