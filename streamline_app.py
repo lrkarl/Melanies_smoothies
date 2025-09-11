42024d4ae7514641f1c752ab326e731d073de764
lrkarl <lkarl@revescoproperties.com>
2025-09-11 15:12:23 -0600

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -41,7 +41,7 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + str(search_on))
         st.json(smoothiefroot_response.json())
 
     st.write(ingredients_string)
@@ -51,7 +51,7 @@ if ingredients_list:
 
     st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
-    
+
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered!', icon="✅")
