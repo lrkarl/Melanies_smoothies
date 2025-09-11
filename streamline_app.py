3c0f2c339ee71adab79812c0a5d71304b1f938e8
lrkarl <lkarl@revescoproperties.com>
2025-09-11 14:21:15 -0600

    Update streamline_app.py
    First round API

--- streamline_app.py
+++ streamline_app.py
@@ -44,4 +44,6 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!',icon="✅" )  
 
-    
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
